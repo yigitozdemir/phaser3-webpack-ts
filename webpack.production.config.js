6534d08764e1e6d5dca11a73a267973fdc2e7228
Yiğit Özdemir <yigitozdemir@yandex.com>
2018-06-01 15:42:04 +0300

    Production configuration added

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,14 +1,13 @@
 var path = require("path");
 var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
-var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 // Phaser webpack config
 var phaserModule = path.join(__dirname, "/node_modules/phaser/");
 var phaser = path.join(phaserModule, "dist/phaser.min.js");
 
 var definePlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || "true")),
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || "false")),
   WEBGL_RENDERER: true,
   CANVAS_RENDERER: true
 });
@@ -18,43 +17,41 @@ module.exports = {
     app: [path.resolve(__dirname, "src/main.ts")],
     vendor: ["phaser"]
   },
-  devtool: "cheap-source-map",
   output: {
-    pathinfo: true,
     path: path.resolve(__dirname, "bundle"),
     publicPath: "./bundle/",
-    filename: "bundle.js"
+    filename: "bundle.production.js"
   },
-  watch: true,
   plugins: [
     definePlugin,
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.UglifyJsPlugin({
+      drop_console: true,
+      minimize: true,
+      output: {
+        comments: false
+      }
+    }),
     new webpack.optimize.CommonsChunkPlugin({
       name: "vendor" /* chunkName= */,
-      filename: "vendor.bundle.js" /* filename= */
+      filename: "js/vendor.bundle.js" /* filename= */
     }),
     new HtmlWebpackPlugin({
-      filename: "../index.html",
+      filename: "index.html",
       template: "./src/index.html",
       chunks: ["vendor", "app"],
       chunksSortMode: "manual",
       minify: {
-        removeAttributeQuotes: false,
-        collapseWhitespace: false,
-        html5: false,
-        minifyCSS: false,
-        minifyJS: false,
-        minifyURLs: false,
-        removeComments: false,
-        removeEmptyAttributes: false
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        html5: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true,
+        removeComments: true,
+        removeEmptyAttributes: true
       },
-      hash: false
-    }),
-    new BrowserSyncPlugin({
-      host: process.env.IP || "localhost",
-      port: process.env.PORT || 3000,
-      server: {
-        baseDir: ["./", "./build"]
-      }
+      hash: true
     })
   ],
   module: {
